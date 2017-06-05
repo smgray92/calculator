b5c566bda9bda2bbf6de27dd0490341c157dc860
Sharon Gray <smgray113@gmail.com>
2017-06-04 21:03:08 -0700

    add forms to make the page more interactive.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-var add = function(number1, number2) {return number1 + number2; };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-var subtract = function(number1, number2) {return number1 - number2; };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
-var multiply = function(number1, number2) {return number1 * number2; };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-var divide = function(number1, number2) {return number1 / number2; };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
-var BMI = function(number1, number2) { return number1/ (number2 * number2);};
-var number1 = parseInt(prompt("Enter your weight:"));
-var number2 = parseInt(prompt("Enter your height:"));
-var result = BMI(number1, number2);
-alert(result);
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
